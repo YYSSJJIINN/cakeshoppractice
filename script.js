71e236936cd4ae853d2f571a78b2299518804a6c
YYSSJJIINN <h20171058@daum.net>
2025-02-14 17:00:13 +0900

    제출용 내용이랑 합침~

--- script.js
+++ script.js
@@ -39,3 +39,19 @@
         // 돔형 맞춤 케이크 상세 페이지 숨기기
         document.getElementById("watermelonCakeDetail").style.display = "none";
     }
+    
+// 더보기 버튼
+    document.addEventListener("DOMContentLoaded", function () {
+        const moreCakes = document.getElementById("moreCakes");
+        const loadMoreBtn = document.getElementById("loadMoreBtn");
+    
+        loadMoreBtn.addEventListener("click", function () {
+            if (moreCakes.classList.contains("show")) {
+                moreCakes.classList.remove("show");
+                loadMoreBtn.textContent = "더보기";
+            } else {
+                moreCakes.classList.add("show");
+                loadMoreBtn.textContent = "닫기";
+            }
+        });
+    });
